ff1f0c28ecc2a6750dbe1ac542f322b5dcf764fc
ponmanivasahan <vasupks01@gmail.com>
2026-03-05 12:39:06 +0530

    developed how to play div

---
+++ js/characterClasses.js
@@ -0,0 +1,171 @@
+class CharacterClass {
+  constructor({ name, stats, color, secondaryColor }) {
+    this.name = name;
+    this.stats = stats;
+    this.color = color;
+    this.secondaryColor = secondaryColor;
+  }
+
+  drawIcon(ctx, x, y, size) {
+    ctx.save();
+    switch(this.name) {
+      case 'Warrior':
+        this.drawWarriorIcon(ctx, x, y, size);
+        break;
+      case 'Archer':
+        this.drawArcherIcon(ctx, x, y, size);
+        break;
+      case 'Mage':
+        this.drawMageIcon(ctx, x, y, size);
+        break;
+      case 'Tank':
+        this.drawTankIcon(ctx, x, y, size);
+        break;
+    }
+    ctx.restore();
+  }
+
+  drawWarriorIcon(ctx, x, y, size) {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(x + size * 0.4, y + size * 0.2, size * 0.2, size * 0.6);
+    ctx.fillStyle = this.secondaryColor;
+    ctx.fillRect(x + size * 0.35, y + size * 0.15, size * 0.3, size * 0.1);
+    ctx.beginPath();
+    ctx.moveTo(x + size * 0.5, y);
+    ctx.lineTo(x + size * 0.4, y + size * 0.2);
+    ctx.lineTo(x + size * 0.6, y + size * 0.2);
+    ctx.closePath();
+    ctx.fill();
+  }
+
+  drawArcherIcon(ctx, x, y, size) {
+    ctx.strokeStyle = this.color;
+    ctx.lineWidth = size * 0.08;
+    ctx.beginPath();
+    ctx.arc(x + size * 0.5, y + size * 0.5, size * 0.3, -Math.PI * 0.5, Math.PI * 0.5);
+    ctx.stroke();
+    ctx.fillStyle = this.secondaryColor;
+    ctx.beginPath();
+    ctx.moveTo(x + size * 0.2, y + size * 0.5);
+    ctx.lineTo(x + size * 0.6, y + size * 0.5);
+    ctx.lineTo(x + size * 0.55, y + size * 0.45);
+    ctx.lineTo(x + size * 0.55, y + size * 0.55);
+    ctx.closePath();
+    ctx.fill();
+  }
+
+  drawMageIcon(ctx, x, y, size) {
+    ctx.strokeStyle = this.secondaryColor;
+    ctx.lineWidth = size * 0.06;
+    ctx.beginPath();
+    ctx.moveTo(x + size * 0.5, y + size * 0.2);
+    ctx.lineTo(x + size * 0.5, y + size * 0.8);
+    ctx.stroke();
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(x + size * 0.5, y + size * 0.2, size * 0.15, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.fillStyle = this.color + '44';
+    ctx.beginPath();
+    ctx.arc(x + size * 0.5, y + size * 0.2, size * 0.25, 0, Math.PI * 2);
+    ctx.fill();
+  }
+
+  drawTankIcon(ctx, x, y, size) {
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.moveTo(x + size * 0.5, y);
+    ctx.lineTo(x + size * 0.8, y + size * 0.3);
+    ctx.lineTo(x + size * 0.8, y + size * 0.7);
+    ctx.lineTo(x + size * 0.5, y + size);
+    ctx.lineTo(x + size * 0.2, y + size * 0.7);
+    ctx.lineTo(x + size * 0.2, y + size * 0.3);
+    ctx.closePath();
+    ctx.fill();
+    ctx.fillStyle = this.secondaryColor;
+    ctx.fillRect(x + size * 0.45, y + size * 0.2, size * 0.1, size * 0.6);
+    ctx.fillRect(x + size * 0.3, y + size * 0.45, size * 0.4, size * 0.1);
+  }
+}
+
+const characterClasses = {
+  warrior: new CharacterClass({
+    name: 'Warrior',
+    stats: {
+      baseHealth: 100,
+      baseDamage: 25,
+      attackSpeed: 1.0,
+      moveSpeed: 100,
+      attackRange: 20,
+      description: 'High damage, slow movement'
+    },
+    color: '#ff4444',
+    secondaryColor: '#cc0000'
+  }),
+  
+  archer: new CharacterClass({
+    name: 'Archer',
+    stats: {
+      baseHealth: 70,
+      baseDamage: 15,
+      attackSpeed: 1.5,
+      moveSpeed: 180,
+      attackRange: 80,
+      description: 'Ranged attacks, fast movement'
+    },
+    color: '#44ff44',
+    secondaryColor: '#00cc00'
+  }),
+  
+  mage: new CharacterClass({
+    name: 'Mage',
+    stats: {
+      baseHealth: 60,
+      baseDamage: 30,
+      attackSpeed: 0.8,
+      moveSpeed: 120,
+      attackRange: 60,
+      areaOfEffect: 40,
+      description: 'Area damage, magic attacks'
+    },
+    color: '#4444ff',
+    secondaryColor: '#0000cc'
+  }),
+  
+  tank: new CharacterClass({
+    name: 'Tank',
+    stats: {
+      baseHealth: 150,
+      baseDamage: 10,
+      attackSpeed: 0.7,
+      moveSpeed: 80,
+      attackRange: 18,
+      shieldAbility: true,
+      description: 'High health, shield ability'
+    },
+    color: '#888888',
+    secondaryColor: '#444444'
+  })
+};
+
+let selectedClass = null;
+
+function getSelectedClass() {
+  return selectedClass || characterClasses.warrior;
+}
+
+function setPlayerClass(className) {
+  selectedClass = characterClasses[className];
+  localStorage.setItem('selectedClass', className);
+  return selectedClass;
+}
+
+function loadPlayerClass() {
+  const saved = localStorage.getItem('selectedClass');
+  if (saved && characterClasses[saved]) {
+    selectedClass = characterClasses[saved];
+  } else {
+    selectedClass = characterClasses.warrior;
+  }
+  return selectedClass;
+}
---
+++ js/classes.js
@@ -0,0 +1,261 @@
+class Sprite {
+  constructor({
+    position,
+    imageSrc,
+    scale = 1,
+    framesMax = 1,
+    offset = { x: 0, y: 0 },
+    facing = 1
+  }) {
+    this.position = position;
+    this.width = 50;
+    this.height = 150;
+    this.image = new Image();
+    this.image.src = imageSrc;
+    this.scale = scale;
+    this.framesMax = framesMax;
+    this.framesCurrent = 0;
+    this.framesElapsed = 0;
+    this.framesHold = 5;
+    this.offset = offset;
+    this.facing = facing;
+  }
+
+  draw() {
+    if (!window.c) return;
+    const ctx = window.c;
+    const frameWidth = this.image.width / this.framesMax;
+    const frameHeight = this.image.height;
+    const drawWidth = frameWidth * this.scale;
+    const drawHeight = frameHeight * this.scale;
+    const drawX = this.position.x - this.offset.x;
+    const drawY = this.position.y - this.offset.y;
+
+    if (this.facing === -1) {
+      ctx.save();
+      ctx.scale(-1, 1);
+      ctx.drawImage(
+        this.image,
+        this.framesCurrent * frameWidth,
+        0,
+        frameWidth,
+        frameHeight,
+        -(drawX + drawWidth),
+        drawY,
+        drawWidth,
+        drawHeight
+      );
+      ctx.restore();
+      return;
+    }
+
+    ctx.drawImage(
+      this.image,
+      this.framesCurrent * frameWidth,
+      0,
+      frameWidth,
+      frameHeight,
+      drawX,
+      drawY,
+      drawWidth,
+      drawHeight
+    );
+  }
+
+  animateFrames() {
+    this.framesElapsed++;
+    if (this.framesElapsed % this.framesHold === 0) {
+      if (this.framesCurrent < this.framesMax - 1) {
+        this.framesCurrent++;
+      } else {
+        this.framesCurrent = 0;
+      }
+    } 
+  }
+
+  update() {
+    this.draw();
+    this.animateFrames();
+  }
+}
+
+class Fighter extends Sprite {
+  constructor({
+    position,
+    velocity,
+    color = 'red',
+    imageSrc,
+    scale = 1,
+    framesMax = 1,
+    offset = { x: 0, y: 0 },
+    sprites,
+    facing = 1,
+    attackBox = { offset: {}, width: undefined, height: undefined }
+  }) {
+    super({
+      position,
+      imageSrc,
+      scale,
+      framesMax,
+      offset,
+      facing
+    });
+    this.velocity = velocity;
+    this.width = 50;
+    this.height = 150;
+    this.lastKey;
+    this.attackBox = {
+      position: {x: this.position.x, y: this.position.y},
+      offset: attackBox.offset,
+      width: attackBox.width,
+      height: attackBox.height
+    };
+    this.color = color;
+    this.isAttacking = false;
+    this.health = 300;
+    this.framesCurrent = 0;
+    this.framesElapsed = 0;
+    this.framesHold = 5;
+    this.sprites = sprites;
+    this.dead = false;
+    this.combo = 0;
+    this.maxCombo = 0;
+    this.isBlocking = false;
+    this.specialCharge = 0;
+    this.specialChargeMax = 100;
+    this.isSpecialAttack = false;
+    
+    for (const sprite in this.sprites) {
+      if (this.sprites[sprite]) {
+        this.sprites[sprite].image = new Image();
+        this.sprites[sprite].image.src = this.sprites[sprite].imageSrc;
+      }
+    }
+  }
+
+  update() {
+    this.draw();
+    if (!this.dead) this.animateFrames();
+    this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
+    this.attackBox.position.y = this.position.y + this.attackBox.offset.y;
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+    
+    if (this.position.x < 0) {
+      this.position.x = 0;
+      this.velocity.x = 0;
+    }
+    if (this.position.x + this.width > window.canvas.width) {
+      this.position.x = window.canvas.width - this.width;
+      this.velocity.x = 0;
+    }
+    if (this.position.y < 50) {
+      this.position.y = 50;
+      this.velocity.y = 0;
+    }
+    
+    if (this.position.y + this.height + this.velocity.y >= window.canvas.height - 96) {
+      this.velocity.y = 0;
+      this.position.y = 330;
+    } else this.velocity.y += window.gravity;
+  }
+
+  attack() {
+    this.switchSprite('attack1');
+    this.isAttacking = true;
+  }
+
+  takeHit(isCritical = false, damage = null) {
+    const dmg = damage !== null ? damage : (isCritical ? 40 : 20);
+    this.health -= dmg;
+    this.combo = 0;
+
+    if (this.health <= 0) {
+      this.switchSprite('death');
+    } else this.switchSprite('takeHit');
+  }
+
+  specialAttack() {
+    if (this.specialCharge < this.specialChargeMax || this.isAttacking || this.dead) return false;
+    this.isAttacking = true;
+    this.isSpecialAttack = true;
+    this.specialCharge = 0;
+    this.switchSprite('attack1');
+    setTimeout(() => {
+      this.isSpecialAttack = false;
+    }, 600);
+    return true;
+  }
+
+  switchSprite(sprite) {
+    if (this.image === this.sprites.death.image) {
+      if (this.framesCurrent === this.sprites.death.framesMax - 1)
+        this.dead = true;
+      return;
+    }
+    
+    if (
+      this.image === this.sprites.attack1.image &&
+      this.framesCurrent < this.sprites.attack1.framesMax - 1
+    )
+      return;
+      
+    if (
+      this.image === this.sprites.takeHit.image &&
+      this.framesCurrent < this.sprites.takeHit.framesMax - 1
+    )
+      return;
+      
+    switch (sprite) {
+      case 'idle':
+        if (this.image !== this.sprites.idle.image) {
+          this.image = this.sprites.idle.image;
+          this.framesMax = this.sprites.idle.framesMax;
+          this.framesCurrent = 0;
+        }
+        break;
+      case 'run':
+        if (this.image !== this.sprites.run.image) {
+          this.image = this.sprites.run.image;
+          this.framesMax = this.sprites.run.framesMax;
+          this.framesCurrent = 0;
+        }
+        break;
+      case 'jump':
+        if (this.image !== this.sprites.jump.image) {
+          this.image = this.sprites.jump.image;
+          this.framesMax = this.sprites.jump.framesMax;
+          this.framesCurrent = 0;
+        }
+        break;
+      case 'fall':
+        if (this.image !== this.sprites.fall.image) {
+          this.image = this.sprites.fall.image;
+          this.framesMax = this.sprites.fall.framesMax;
+          this.framesCurrent = 0;
+        }
+        break;
+      case 'attack1':
+        if (this.image !== this.sprites.attack1.image) {
+          this.image = this.sprites.attack1.image;
+          this.framesMax = this.sprites.attack1.framesMax;
+          this.framesCurrent = 0;
+        }
+        break;
+      case 'takeHit':
+        if (this.image !== this.sprites.takeHit.image) {
+          this.image = this.sprites.takeHit.image;
+          this.framesMax = this.sprites.takeHit.framesMax;
+          this.framesCurrent = 0;
+        }
+        break;
+      case 'death':
+        if (this.image !== this.sprites.death.image) {
+          this.image = this.sprites.death.image;
+          this.framesMax = this.sprites.death.framesMax;
+          this.framesCurrent = 0;
+        }
+        break;
+    }
+  }
+}
